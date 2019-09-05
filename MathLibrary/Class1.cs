f714889704774f0f4ebb6d9b8ad27ba886e71dbf
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-09-05 15:25:07 +0530

    Add files via upload

---
+++ MathLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathComponent
+    {
+        public double AddFunc(double v1, double v2) => v1 + v2;
+        public double SubFunc(double v1, double v2) => v1 - v2;
+        public double MulFunc(double v1, double v2) => v1 * v2;
+        public double DivFunc(double v1, double v2) => v1 / v2;
+        public double Square(double v) => v * v;
+        public double SquareRoot(double v) => Math.Sqrt(v);
+    }
+}
