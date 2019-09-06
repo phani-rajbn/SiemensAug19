97653949fb0ad8901e8b25e354f43448535e4d35
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-09-06 16:53:44 +0530

    Add files via upload

--- SampleWpfApp/Repository.xaml.cs
+++ SampleWpfApp/Repository.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using SampleDll;
 namespace SampleWpfApp
 {
     /// <summary>
@@ -19,9 +20,35 @@ namespace SampleWpfApp
     /// </summary>
     public partial class Repository : Window
     {
+        static EntitesComponent component = new EntitesComponent();
+        public EmpTable newEmployee { get; set; }
         public Repository()
         {
             InitializeComponent();
+            newEmployee = new EmpTable();
+            grdNew.DataContext = newEmployee;
+        }
+        public ObservableCollection<EmpTable> Employees { get; set; }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            List<EmpTable> list = component.GetAllEmployees();
+            Employees = new ObservableCollection<EmpTable>(list);
+            lstNames.ItemsSource = Employees;
+            lstNames.DisplayMemberPath = "Empname";
+        }
+        
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var emp = stkSelected.DataContext as EmpTable;
+            component.UpdateEmployee(emp);
+            MessageBox.Show("Employee updated Successfully");
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            component.AddEmployee(newEmployee);
+            MessageBox.Show("Employee added successfully");
         }
     }
 }
