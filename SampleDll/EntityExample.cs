25af2e989bff8c9c67b7c3047e66cfc7da1f4b1a
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-09-06 11:36:03 +0530

    Add files via upload

---
+++ SampleDll/EntityExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDll
+{
+    public interface IEntitiesComponent
+    {
+        void AddEmployee(EmpTable emp);
+        void UpdateEmployee(EmpTable emp);
+        void DeleteEmployee(int id);
+        List<EmpTable> GetAllEmployees();
+    }
+
+    public class EntitesComponent : IEntitiesComponent
+    {
+        public void AddEmployee(EmpTable emp)
+        {
+            //No SQL Statements, everything is OOP....
+            var context = new MyDBEntities();
+            context.EmpTables.Add(emp);//Adds to the Collection...
+            context.SaveChanges();//Commit to the database..
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var context = new MyDBEntities();
+            var emp = context.EmpTables.FirstOrDefault((e) => e.EmpID == id);
+            if (emp == null) throw new Exception("No Employee was found to delete");
+            context.EmpTables.Remove(emp);
+            context.SaveChanges();
+        }
+
+        public List<EmpTable> GetAllEmployees()
+        {
+            return new MyDBEntities().EmpTables.ToList();
+        }
+
+        public void UpdateEmployee(EmpTable emp)
+        {
+            throw new NotImplementedException("Do it urself...");
+        }
+    }
+}
---
+++ SampleWpfApp/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWpfApp.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public DateTime BillDate { get; set; }
+        public double BillAmount { get; set; }
+    }
+}
