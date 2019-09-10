26108ce0c49182d6fa8e11306e74eed89c2a74b6
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-09-10 19:43:29 +0530

    Add files via upload

---
+++ SampleWebApi/Controllers/TravelogueController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    using SampleWebApi.Models;
+    public class TravelogueController : ApiController
+    {
+        public List<Travelogue> GetAll()
+        {
+            var context = new SiemensDBEntities();
+            return context.Travelogues.ToList();
+        }
+
+        public bool PostNewBlog(Travelogue travelogue)
+        {
+            var context = new SiemensDBEntities();
+            context.Travelogues.Add(travelogue);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
